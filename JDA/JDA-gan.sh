86d0f780a9f292d292b6848b6098087fc0f0dbc5
Weixin Liu <weixinli@matrix.ml.cmu.edu>
2020-02-27 22:39:26 -0500

    jda-gan.sh

---
+++ JDA/JDA-gan.sh
@@ -0,0 +1,12 @@
+!# /bin/bash
+
+python JDA-gan.py --data_path /home/weixinli \
+		  --task 3A \
+		  --batch_size 400 \
+                  --epochs 10 \
+                  --gap 4 \
+                  --lbl_percentage 0.2 \
+                  --num_per_class -1 \
+                  --classifier /home/weixinli/time-series-domain-adaptation/JDA/FNN_trained_model \
+                  --save_path ../train_related/JDA_GAN \
+                  --model_save_period 5 
