ac9805b7b48072790f8f9883d7d24d7f6dbb3d72
stevenliu000 <wxabc147@hotmail.com>
2020-03-12 19:13:07 -0400

    GAN GNet

--- spring-break/GAN.py
+++ spring-break/GAN.py
@@ -5,23 +5,23 @@ class Generator(nn.Module):
     def __init__(self, dim):
         super(Generator, self).__init__()
         self.net = nn.Sequential(
-            nn.Linear(dim, 300),
-            nn.LayerNorm(300),
+            nn.Linear(dim, dim),
+            nn.LayerNorm(dim),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Linear(300, 500),
-            nn.LayerNorm(500),
+            nn.Linear(dim, dim),
+            nn.LayerNorm(dim),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Linear(500, 500),
-            nn.LayerNorm(500),
+            nn.Linear(dim, dim),
+            nn.LayerNorm(dim),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Linear(500, 300),
-            nn.LayerNorm(300),
+            nn.Linear(dim, dim),
+            nn.LayerNorm(dim),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Linear(300, dim),
+            nn.Linear(dim, dim),
             nn.LayerNorm(dim),
         ) 
 
