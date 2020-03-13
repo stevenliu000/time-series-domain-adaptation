d3553aa9cf70481e297e0a36a42d29e35e100498
stevenliu000 <wxabc147@hotmail.com>
2020-03-13 00:07:03 -0400

    stage3_local_gp

--- spring-break/stage3_local.py
+++ spring-break/stage3_local.py
@@ -285,40 +285,6 @@ def encoder_inference(encoder, x):
     return torch.cat((real[:,-1,:], imag[:,-1,:]), -1).reshape(x.shape[0], -1)
 
 
-# In[ ]:
-
-
-def _gradient_penalty(self, real_data, generated_data):
-    batch_size = real_data.size()[0]
-
-    # Calculate interpolation
-    alpha = torch.rand(batch_size, 1, 1, 1)
-    alpha = alpha.expand_as(real_data)
-    alpha = alpha.to(device)
-    interpolated = alpha * real_data.data + (1 - alpha) * generated_data.data
-    interpolated = Variable(interpolated, requires_grad=True)
-    interpolated = interpolated.to(device)
-
-    # Calculate probability of interpolated examples
-    prob_interpolated = self.D(interpolated)
-
-    # Calculate gradients of probabilities with respect to examples
-    gradients = torch_grad(outputs=prob_interpolated, inputs=interpolated,
-                           grad_outputs=torch.ones(prob_interpolated.size()).to(device)
-                           create_graph=True, retain_graph=True)[0]
-
-    # Gradients have shape (batch_size, num_channels, img_width, img_height),
-    # so flatten to easily take norm per example in batch
-    gradients = gradients.view(batch_size, -1)
-
-    # Derivatives of the gradient close to 0 can cause problems because of
-    # the square root, so manually calculate norm and add epsilon
-    gradients_norm = torch.sqrt(torch.sum(gradients ** 2, dim=1) + 1e-12)
-
-    # Return gradient penalty
-    return args.gp_weight * ((gradients_norm - 1) ** 2).mean()
-
-
 # # Train
 
 # In[14]:
