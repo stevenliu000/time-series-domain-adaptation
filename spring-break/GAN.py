269a97de2dcfa8c4822ba11af2ec03a9f18c2c2f
stevenliu000 <wxabc147@hotmail.com>
2020-03-25 20:54:40 -0400

    prototypeloss step b

--- spring-break/GAN.py
+++ spring-break/GAN.py
@@ -5,23 +5,23 @@ class Generator(nn.Module):
     def __init__(self, dim):
         super(Generator, self).__init__()
         self.net = nn.Sequential(
-            nn.Linear(dim, dim),
-            nn.LayerNorm(dim),
+            nn.Linear(dim, 300),
+            nn.LayerNorm(300),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Linear(dim, dim),
-            nn.LayerNorm(dim),
+            nn.Linear(300, 500),
+            nn.LayerNorm(500),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Linear(dim, dim),
-            nn.LayerNorm(dim),
+            nn.Linear(500, 500),
+            nn.LayerNorm(500),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Linear(dim, dim),
-            nn.LayerNorm(dim),
+            nn.Linear(500, 300),
+            nn.LayerNorm(300),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Linear(dim, dim),
+            nn.Linear(300, dim),
             nn.LayerNorm(dim),
         ) 
 
