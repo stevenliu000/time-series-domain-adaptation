66448cb212dda4efdc3c772c6a6430d80ad66ec9
Tianqin Li <jacklitianqin@gmail.com>
2020-03-26 18:16:00 -0400

    delete local only

--- prototypeloss/step_c.py
+++ prototypeloss/step_c.py
@@ -115,36 +115,36 @@ args = parser.parse_args()
 # In[6]:
 
 
-# local only
-class local_args:
-    def __init__(self, **entries):
-        self.__dict__.update(entries)
+# # local only
+# class local_args:
+#     def __init__(self, **entries):
+#         self.__dict__.update(entries)
         
-args = local_args(**{
-    'data_path': '/Users/tianqinli/Code/Russ/time-series-domain-adaptation/data_unzip',
-    'task': '3E',
-    'num_class': 50,
-    'batch_size': 100,
-    'num_per_class': -1,
-    'gap': 5,
-    'lbl_percentage':0.7,
-    'lr_gan': 1e-4,
-    'lr_FNN': 1e-4,
-    'lr_encoder': 1e-4,
-    'epochs': 2,
-    'clip_value': 0.01,
-    'n_critic': 4,
-    'sclass': 0.7,
-    'scent': 1e-2,
-    'seed': None,
-    'save_path': '/Users/tianqinli/Code/Russ/time-series-domain-adaptation/train_related',
-    'model_save_period': 1,
-    'lr_centerloss': 1e-3,
-    'lr_prototype': 1e-3,
-    'sprototype': 1e-2,
-    'seed': 0,
-    'select_pretrain_epoch': 77,
-})
+# args = local_args(**{
+#     'data_path': '/Users/tianqinli/Code/Russ/time-series-domain-adaptation/data_unzip',
+#     'task': '3E',
+#     'num_class': 50,
+#     'batch_size': 100,
+#     'num_per_class': -1,
+#     'gap': 5,
+#     'lbl_percentage':0.7,
+#     'lr_gan': 1e-4,
+#     'lr_FNN': 1e-4,
+#     'lr_encoder': 1e-4,
+#     'epochs': 2,
+#     'clip_value': 0.01,
+#     'n_critic': 4,
+#     'sclass': 0.7,
+#     'scent': 1e-2,
+#     'seed': None,
+#     'save_path': '/Users/tianqinli/Code/Russ/time-series-domain-adaptation/train_related',
+#     'model_save_period': 1,
+#     'lr_centerloss': 1e-3,
+#     'lr_prototype': 1e-3,
+#     'sprototype': 1e-2,
+#     'seed': 0,
+#     'select_pretrain_epoch': 77,
+# })
 
 
 # In[7]:
