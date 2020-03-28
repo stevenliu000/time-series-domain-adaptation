ad922db87bec3d7a827d81465ba6c0af7549e355
Tianqin Li <jacklitianqin@gmail.com>
2020-03-28 14:33:57 -0400

    add dropout before last layer

--- spring-break/FNN.py
+++ spring-break/FNN.py
@@ -7,21 +7,22 @@ class FNN(nn.Module):
         self.fc1 = nn.Linear(d_in, d_h1)
         self.ln1 = nn.LayerNorm(d_h1)
         self.relu1 = nn.ReLU(inplace=True)
-        self.dropout = nn.Dropout(dp)
+        self.dropout1 = nn.Dropout(dp)
         self.fc2 = nn.Linear(d_h1, d_h2)
         self.ln2 = nn.LayerNorm(d_h2)
         self.relu2 = nn.ReLU(inplace=True)
+        self.dropout2 = nn.Dropout(dp)
         self.fc3 = nn.Linear(d_h2, d_out)
 
     def forward(self, x):
         x = self.fc1(x)
         x = self.ln1(x)
         x = self.relu1(x)
-        x = self.dropout(x)
+        x = self.dropout1(x)
         x = self.fc2(x)
         x = self.ln2(x)
         x = self.relu2(x)
-        x = self.dropout(x)
+        x = self.dropout2(x)
         x = self.fc3(x)
 
         return x
