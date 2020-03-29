3aab97b91940dc96c01a6e76e70795a91f271342
Tianqin Li <jacklitianqin@gmail.com>
2020-03-28 23:17:34 -0400

    change binaryloss

--- Linear Classifier/binaryloss.py
+++ Linear Classifier/binaryloss.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[35]:
+# In[58]:
 
 
-import torch.nn.functional as F
 import torch
 
 
 
-class BinaryLoss(torch.nn.CrossEntropyLoss):
+class BinaryLoss(torch.nn.Module):
     def __init__(self):
         super().__init__()
     
@@ -36,8 +35,22 @@ class BinaryLoss(torch.nn.CrossEntropyLoss):
         # same class label 1, else 0
         class_same = (target_y_rand == source_y_rand).long()
         
-        return F.cross_entropy(logit_all, class_same, weight=self.weight,
-                               ignore_index=self.ignore_index, reduction=self.reduction)
+        criterion_crossentropy = torch.nn.CrossEntropyLoss()
+        loss = criterion_crossentropy(logit_all, class_same)
+        return loss
 
-        
+
+# In[59]:
+
+
+# a = torch.randn((2,3), requires_grad=True)
+# b = torch.randn((2,3), requires_grad=True)
+
+# a_lbl = torch.randint(0,10,(2,))
+# b_lbl = torch.randint(0,10,(2,))
+# device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+# m = BinaryLoss().to(device)
+# loss = m(a, a_lbl, b, b_lbl)
+# loss.backward()
+# loss
 
