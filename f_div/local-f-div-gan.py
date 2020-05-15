local KL fix

f_div/local-f-div-gan.py
@@ -311,8 +311,8 @@ def JSDiv(g_x_source, g_x_target, mask_source, mask_target, device):
     t_score = F.softplus(g_x_target) * mask_target # (batch_size, num_class)
 
     # E_{p(x|y)}
-    s_score = s_score.mean(dim=0) # (num_class, )
-    t_score = t_score.mean(dim=0) # (num_class, )
+    s_score = s_score.sum(dim=0) / mask_source.sum(dim=0) # (num_class, )
+    t_score = t_score.sum(dim=0) / mask_source.sum(dim=0) # (num_class, )
 
     # E_{p(y)}
     return (- s_score - t_score).mean() # scalar