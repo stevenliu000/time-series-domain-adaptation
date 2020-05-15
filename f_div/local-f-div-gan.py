74ac1f52783328cc3d3dcf9780d86d29fc8015bd
stevenliu000 <wxabc147@hotmail.com>
2020-05-14 20:58:56 -0400

    local-gan

--- f_div/local-f-div-gan.py
+++ f_div/local-f-div-gan.py
@@ -480,6 +480,7 @@ for epoch in range(start_epoch, end_epoch, args.intervals*args.model_save_period
             target_x_unlabeled_embedding = torch.cat([target_x_unlabeled_embedding, fake_x_embedding])
             target_y_unlabeled = torch.cat([target_y_unlabeled, target_y])
 
+    print(source_y_labeled.shape)
     mask_source_labeled = torch.zeros((source_y_labeled.size(0), num_class), device=device).scatter_(1, source_y_labeled, 1)
     mask_target_labeled = torch.zeros((target_y_labeled.size(0), num_class), device=device).scatter_(1, target_y_labeled, 1)
     mask_source_unlabeled = torch.zeros((source_y_unlabeled.size(0), num_class), device=device).scatter_(1, source_y_unlabeled, 1)
