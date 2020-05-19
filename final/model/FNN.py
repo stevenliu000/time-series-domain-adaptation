341f9b737baba70aedc43c333944675c2817132a
stevenliu000 <wxabc147@hotmail.com>
2020-05-19 14:10:37 -0400

    final

---
+++ final/model/FNN.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+
+class FNN(nn.Module):
+    def __init__(self, d_in, d_h1, d_h2, d_out, dp=0.2):
+        super(FNN, self).__init__()
+        self.fc1 = nn.Linear(d_in, d_h1)
+        self.ln1 = nn.LayerNorm(d_h1)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.dropout1 = nn.Dropout(dp)
+        self.fc2 = nn.Linear(d_h1, d_h2)
+        self.ln2 = nn.LayerNorm(d_h2)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.dropout2 = nn.Dropout(dp)
+        self.fc3 = nn.Linear(d_h2, d_out)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.ln1(x)
+        x = self.relu1(x)
+        x = self.dropout1(x)
+        x = self.fc2(x)
+        x = self.ln2(x)
+        x = self.relu2(x)
+        x = self.dropout2(x)
+        x = self.fc3(x)
+
+        return x
